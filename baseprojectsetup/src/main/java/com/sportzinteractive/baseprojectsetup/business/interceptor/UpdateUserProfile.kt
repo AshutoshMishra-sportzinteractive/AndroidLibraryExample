da45b0bd4908d341d06c77b2b80848059828365c
Ashutosh.Mishra <ashutosh.mishra@sportzinteractive.net>
2023-08-02 21:23:42 +0530

    UpdateProfile Api Added

---
+++ baseprojectsetup/src/main/java/com/sportzinteractive/baseprojectsetup/business/interceptor/UpdateUserProfile.kt
@@ -0,0 +1,16 @@
+package com.sportzinteractive.baseprojectsetup.business.interceptor
+
+import com.sportzinteractive.baseprojectsetup.data.model.auth.UpdateUserProfileRequest
+import com.sportzinteractive.baseprojectsetup.data.model.auth.UpdateUserProfileState
+import com.sportzinteractive.baseprojectsetup.data.repository.AuthRepository
+import com.sportzinteractive.baseprojectsetup.helper.Resource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UpdateUserProfile @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(updateUserProfileRequest: UpdateUserProfileRequest, url:String): Flow<Resource<UpdateUserProfileState?>> {
+        return authRepository.updateUserProfile(updateUserProfileRequest,url)
+    }
+}
